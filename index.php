6edce1409b06da3fa2856eb21ad4909ae37968eb
sehemu92 <sehemu@alumnatflorida.com>
2025-02-06 20:35:00 +0100

    Noveno commit

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     </head>
     <body>
         <div class="container">
-        <h1>Generador aleatorio de Hola Mundos commit 8</h1>
+        <h1>Generador aleatorio de Hola Mundos commit 9</h1>
         <form method="post">
         <button type="submit">Generar</button>
         </form>
