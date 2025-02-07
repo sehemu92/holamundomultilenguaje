f5786bb347d607bd23bf76139e03290cab0cf06b
sehemu92 <sehemu@alumnatflorida.com>
2025-02-07 14:01:56 +0100

    Onceavo commit

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     </head>
     <body>
         <div class="container">
-        <h1>Generador aleatorio de Hola Mundos con propietario UBUNTU de la carpeta "holamundo"</h1>
+        <h1>Generador aleatorio de Hola Mundos con propietario UBUNTU de la carpeta "holamundo" con instalacion de composer</h1>
         <form method="post">
         <button type="submit">Generar</button>
         </form>
